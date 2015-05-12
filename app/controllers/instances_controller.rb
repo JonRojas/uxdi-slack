190e3eb5f4139d361ad635aced12078b30a23cbd
JonRojas <jonathan.rojas@gmail.com>
2015-05-12 17:13:04 -0400

    Edited controllers (instances and weeks) and edited views

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -1,9 +1,12 @@
 class InstancesController < ApplicationController
 
 	def index
+		@instances = Instance.all
 	end
 
 	def show
+		@instance = Instance.find(params[:id])
+		@weeks = @instance.weeks
 	end
 
 	def create
---
+++ app/controllers/weeks_controller.rb
@@ -0,0 +1,20 @@
+class WeeksController < ApplicationController
+
+def index
+	
+end
+
+def show
+	@week = Instance.first.weeks
+end
+
+def create
+end
+
+def new
+end
+
+def edit
+end
+
+end
